b454d9dc2775cff28432ee372d064374d7d4b57f
Bob Childress <bob.childress@cox.net>
2022-11-17 23:30:44 +0000

    Bob Test File Commit

--- tests/test_character.py
+++ tests/test_character.py
@@ -5,11 +5,12 @@ class Character(TestCase):
     def test_init(self):
         test_controller = Character()
         expected_class = Character()
+        self.assertion
 
 # Test if Init Method Exist
     def test_init(self):
-        test_controller = Init()  
-        expected_method = Init()  
+        #test_controller = Init()  
+        #expected_method = Init()  
         
 #Test getName Method exists
 
