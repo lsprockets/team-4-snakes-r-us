178cd79a8308ceedd1deb7cd4e4ff3601089c262
Aditya Chaudhari <adityacha1305@gmail.com>
2022-11-17 22:31:37 +0000

    set postion, set map

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -17,4 +17,21 @@ class Character:
         m = int(input())
         return m
 
+    def setMap(m):
+        # border conditions
+        m += 2
+        
+        map = [[0] * m for m in range(m)]
+        for i in range(1,len(map)-1):
+            for j in range(1,len(map)-1):
+                map[i][j] = count + 1
+                count +=1
+        return map
 
+    def setPosition():
+        curr_r = 1
+        curr_c = 1
+        curr_pos = [curr+r][curr_c]
+        return curr_pos
+
+        
