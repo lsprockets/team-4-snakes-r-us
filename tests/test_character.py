6b93bd982ed1dd90b93acdc2eb6879c252df2205
Bob Childress <bob.childress@cox.net>
2022-11-17 23:22:05 +0000

    Bob Test File Update

--- tests/test_character.py
+++ tests/test_character.py
@@ -6,15 +6,11 @@ class Character(TestCase):
         test_controller = Character()
         expected_class = Character()
 
-# Test if Init Meethod Exist
-    #def init(self):
-        #test_controller = Init()    
+# Test if Init Method Exist
+    def test_init(self):
+        test_controller = Init()  
+        expected_method = Init()  
         
-#Test Character Class Exists
-    #def test_char_has_name
-
-#Test Init Method exists
-
 #Test getName Method exists
 
 #Test enterMap Method exists
