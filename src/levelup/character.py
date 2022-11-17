6eff7ac9357cf501099e4d7d38464a224a16dcb6
Aditya Chaudhari <adityacha1305@gmail.com>
2022-11-17 21:31:27 +0000

    init, getName, enter_Map

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -4,3 +4,17 @@ from dataclasses import dataclass
 @dataclass
 class Character:
     name: str
+
+    # Init method
+    def __init__(self,name):
+        self.name = name
+
+    def getName():
+        name = input("Enter Character Name")
+        return name
+
+    def enter_Map():
+        m = int(input())
+        return m
+
+
